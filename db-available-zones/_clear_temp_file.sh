7924fc25ed6cd857ec97ffeadc07f79d58ded8c1
Xiang Zhou <junlan.zx@alibaba-inc.com>
2021-04-13 20:12:29 +0800

    Move db-available-zones project

--- db-available-zones/_clear_temp_file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm -rf */.terraform
-rm -rf */.terraform.lock.hcl
-rm -rf */terraform.tfstate
-rm -rf */terraform.tfstate.backup
--- db-available-zones/alibabacloud-regions/main.tf
+++
@@ -1,9 +0,0 @@
-provider "alicloud" {
-  #   access_key = "${var.access_key}"
-  #   secret_key = "${var.secret_key}"
-}
-
-data "alicloud_regions" "current_region_ds" {
-  current     = false
-  output_file = "AlibabaCloud-Available-Regions.txt"
-}
